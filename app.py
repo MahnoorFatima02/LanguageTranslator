89872605f1fda503bf65c25fb186fa1f3156013e
Mahnoor Fatima <89933726+MahnoorFatima02@users.noreply.github.com>
2025-02-24 17:19:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import os
 
 app = Flask(__name__)
 
+# Initialize the translation model once at startup
+translator = pipeline(task="translation", model="Helsinki-NLP/opus-mt-en-fi")
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -13,8 +16,9 @@ def index():
 def submit():
     data = request.form['translation_text']
     print("Translation request recieved for " + data)
-    translator = pipeline(task="translation", model="Helsinki-NLP/opus-mt-en-fi")
     translated_text = translator(data)
+    # translator = pipeline(task="translation", model="Helsinki-NLP/opus-mt-en-fi")
+    # translated_text = translator(data)
     return jsonify({"translated_text": translated_text[0]["translation_text"]})
 
 # if __name__ == '__main__':
